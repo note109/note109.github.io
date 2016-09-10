3812543d67ec610571bbe0dd594c789ae5ce2061
MasatoHayakawa <MasatoHayakawa@EMP-MAC-tanishi.local>
2016-09-10 18:58:27 +0900

    :sparkles: add media query

--- develop/app/components/views/Index/style.js
+++ develop/app/components/views/Index/style.js
@@ -1,5 +1,9 @@
 import {Style} from "app/utils/style.js";
+import {isPC} from "app/constants/style.js";
 
 export const TEXT = Style.registerStyle({
   color: "#efefef",
+  [isPC]: {
+    color: "red",
+  },
 });
---
+++ develop/app/constants/style.js
@@ -0,0 +1,1 @@
+export const isPC = "@media (min-width: 768px)";
