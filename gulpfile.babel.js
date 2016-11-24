71eec42ac06b0bfb7cad00377536251193bcb4e7
MasatoHayakawa <MasatoHayakawa@EMP-MAC-tanishi.local>
2016-11-25 01:22:27 +0900

    :shirt: quote style

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,7 @@ const $ = gulpLoadPlugins();
 gulp.task("webserver", function() {
   gulp.src("./")
     .pipe($.webserver({
-      host: '0.0.0.0',
+      host: "0.0.0.0",
       livereload: true,
     }));
 });
