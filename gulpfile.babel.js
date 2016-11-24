33b3e1ad0fcfc69e15bb246e31748dab4a643c73
MasatoHayakawa <MasatoHayakawa@EMP-MAC-tanishi.local>
2016-11-25 01:21:42 +0900

    :clock12: init dev env

---
+++ gulpfile.babel.js
@@ -0,0 +1,44 @@
+import gulp from "gulp"
+import gulpLoadPlugins from "gulp-load-plugins"
+const $ = gulpLoadPlugins();
+
+gulp.task("webserver", function() {
+  gulp.src("./")
+    .pipe($.webserver({
+      host: '0.0.0.0',
+      livereload: true,
+    }));
+});
+
+gulp.task("assets", () => {
+  gulp.src("./app/assets/**")
+    .pipe($.plumber())
+    .pipe(gulp.dest("dest/assets"))
+});
+
+gulp.task("js", () => {
+  gulp.src("./app/scripts/*.js")
+    .pipe($.plumber())
+    .pipe($.babel())
+    .pipe(gulp.dest("dest/scripts"))
+});
+
+gulp.task("pug", () => {
+  gulp.src("./app/views/*.pug")
+    .pipe($.plumber())
+    .pipe($.pug())
+    .pipe(gulp.dest("dest/views"))
+});
+
+gulp.task("sass", () => {
+  gulp.src("./app/styles/*.scss")
+    .pipe($.plumber())
+    .pipe($.sass())
+    .pipe(gulp.dest("dest/styles"))
+});
+
+gulp.task("default", ["assets", "webserver"], () => {
+  gulp.watch("./app/styles/*.scss", ["sass"]);
+  gulp.watch("./app/views/*.pug", ["pug"]);
+  gulp.watch("./app/scripts/*.js", ["js"]);
+});
